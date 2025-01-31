cf7ce0fb52ef7bd991fcbccc4f25dc79d2d9e318
Ajay Gurung <gooronz.ajay@gmail.com>
2025-01-31 11:14:40 +0545

    added feature: the nav-section currently being displayed is highlighted

--- src/components/nav-bar.jsx
+++ src/components/nav-bar.jsx
@@ -1,27 +1,31 @@
+'use client'
 import Image from 'next/image'
 import Link from 'next/link'
-import React from 'react'
+import { usePathname } from 'next/navigation'
+import React, { useState } from 'react'
 
 const NavBar = () => {
-    let navContents = [['chat','/chat.jpg'], ['updates','/updates.jpg'], ['communities','/communities.jpg'], ['calls','/calls.jpg']]
+    const pathName = usePathname()
+
+    let navContents = [{name:'chat',icon:'/chat.jpg',link:'/chat'}, {name:'updates',icon:'/updates.jpg',link:'/updates'}, {name:'communities',icon:'/communities.jpg',link:'/communities'}, {name:'calls',icon:'/calls.jpg',link:'/calls'}]
   return (
     <div className='p-4'>
         {
-            <div className=' flex flex-col space-y-10'>
-                <Link href='/whatsapp/chat'>
+            <div className=' flex flex-col justify-center items-center space-y-10'>
+                <Link href='/chat' className='w-[40] h-[40]'>
                     <Image 
                         src='https://static-00.iconduck.com/assets.00/whatsapp-icon-495x512-y1nyb5ge.png'
-                        alt="WhatsApp Logo"
-                        width={40}
-                        height={40}
+                        alt="App Logo"
+                        width={100}
+                        height={100}
                         className='cursor-pointer'
                     />
                 </Link>
                 {navContents.map((item) => {
                     return (
-                        <div key={item[0]} className='text-black cursor-pointer'>
-                            <Link href={'/whatsapp/'+item[0]} className=''>
-                                <Image src={item[1]} width={40} height={40} alt={item[0]}/>
+                        <div key={item.name} className={`w-[40] h-[40] p-1 text-black cursor-pointer flex justify-center items-center ${item.link === pathName ? 'border-4 rounded-lg' : null}`}>
+                            <Link href={item.link}>
+                                <Image src={item.icon} width={30} height={30} alt={item.name}/>
                             </Link>
                         </div>
                     )
